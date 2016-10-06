cbef40ae435b99d70c41b6c52847ab0a316dab3d
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-10-06 16:54:20 +0100

    slightly messy loadsing, contarining and collap[ing of mod and obs, and at the end plottig of mod and bs on same subplot. Lots to do

--- plot_scaling_file.py
+++ plot_scaling_file.py
@@ -4,9 +4,22 @@ from   os    import listdir, getcwd, mkdir, path, walk
 from   pylab import sort
 from   pdb   import set_trace as browser
 
+import iris.plot as iplt
+import iris.quickplot as qplt
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
 data_dir = 'data/'
 mod_out = 'ag589/'
 stash_code = 'm01s00i216'
+
+albedo_file = 'qrclim.land'
+albedo_index = 0
+
+
+## Load data
+obs = iris.load(data_dir + albedo_file)[albedo_index]
+
 
 def listdir_path(path):
     from os import listdir
@@ -15,10 +28,34 @@ def listdir_path(path):
     files = [path + i for i in files]
     return files
 
+def define_axes():
+    return plt.axes(projection=ccrs.Robinson())
 
 mod_files = sort(listdir_path(data_dir + mod_out))
 stash_contraint = iris.AttributeConstraint(STASH = stash_code)
+mod = iris.load_cube(mod_files, stash_contraint)
 
-cube = iris.load_cube(mod_files, stash_contraint)
+## Seasonal tile
+
+## Seaonal grid cell
+mod_season = mod.collapsed('pseudo_level', iris.analysis.MEAN)
+
+## Annual tile average
+mod_tile = mod.collapsed('time', iris.analysis.MEAN)
+
+## Annual grid cel avarage
+def plot_aa(dat, plotN):
+    plt.subplot(2, 1, plotN, projection=ccrs.Robinson())
+    qplt.contourf(dat, 25, cmap = "brewer_Greys_09")
+    plt.gca().coastlines()
+
+    
+obs_aa = obs.collapsed('time', iris.analysis.MEAN)
+mod_aa = mod_tile[0,:,:]#.collapsed('pseudo_level', iris.analysis.SUM)
+
+plot_aa(obs_aa, 1)
+plot_aa(mod_aa, 2)
+
+plt.show()
 
 browser()
