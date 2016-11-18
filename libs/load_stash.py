3316e451437fe9748e9a1ed43aa162dbe1fa3f68
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-11-18 16:21:13 +0000

    £just printing stash codes being openedW

--- libs/load_stash.py
+++ libs/load_stash.py
@@ -2,7 +2,11 @@ import iris
 from pdb import set_trace as browser
 
 def load_stash(files, code, name, units = None):
+    print name
+    print code
+
     stash_constraint = iris.AttributeConstraint(STASH = code)
+    
     cube = iris.load_cube(files, stash_constraint)
     cube.var_name = name
     cube.standard_name = None
