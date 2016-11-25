aba9ec53dba78d2766414f617275d6b76cf94bb8
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-11-25 14:23:28 +0000

    adding in wood speed fluxes and changing some diagmnsotics

--- look_at_carbon.py
+++ look_at_carbon.py
@@ -1,16 +1,18 @@
 #############################################################################
 ## cfg                                                                     ##
 #############################################################################
-
+import numpy as np
 ## General
 data_dir = 'data/'
 mod_out = 'ah410/'
+
+yr2sec = 1.0 / (60 * 60 * 24 * 360)
 
 ## Soils
 soil_fignm = 'soil'
 soil_title = 'SOIL_CARBON_POOL'
 soil_units = 'kg m-2'
-soil_names = ['DPM', 'RPM', 'BIO', 'HUM', 'VEGC']
+soil_names = ['DPM'       , 'RPM'       , 'BIO'       , 'HUM'       , 'VEGC']
 soil_codes = ['m01s19i021', 'm01s19i022', 'm01s19i023', 'm01s19i024', 'm01s19i002']
 soil_cmap  = 'brewer_GnBu_09'
 
@@ -18,18 +20,27 @@ soil_cmap  = 'brewer_GnBu_09'
 Wood_fignm = 'woodProd'
 Wood_title = 'WOOD_PRODUCT'
 Wood_units = 'kg m-2'
-Wood_names = ['FAST', 'MEDIUM', 'SLOW']
+Wood_names = ['FAST'      , 'MEDIUM'    , 'SLOW']
 Wood_codes = ['m01s19i032', 'm01s19i033', 'm01s19i034']
 Wood_cmap  = 'brewer_YlOrBr_09'
 
-yr2sec = 4.0 / (60.0 * 60.0 * 25.0 * 360.0)
+## Wood Fluxes
+WdFl_fignm = 'Wood_fluxes'
+WdFl_title = 'WOOD FLUXES'
+WdFl_units = 'kg m-2 s-1'
+WdFl_names = ['FAST-IN'   , 'MEDIUM-IN' , 'SLOW-IN'   , 'FAST-OUT'  , 'MEDIUM-OUT', 'SLOW-OUT']
+WdFl_codes = ['m01s19i036', 'm01s19i037', 'm01s19i038', 'm01s19i039', 'm01s19i040', 'm01s19i041']
+WdFl_scale = yr2sec * np.array([1.0, 1.0, 1.0, -1.0, -1.0, -1.0])
+WdFl_cmap  = 'brewer_BrBG_11'
+
+
 ## Fluxes
 Flux_fignm = 'Fluxes'
 Flux_title = 'FLUXES'
 Flux_units = 'kg m-2 s-1'
-Flux_names = ['NPP', 'Resp', 'Wood_prod']
-Flux_scale = [yr2sec, -yr2sec, -yr2sec]
+Flux_names = ['NPP'       , 'Resp', 'Wood_prod']
 Flux_codes = ['m01s19i009', 'm01s19i011', 'm01s19i010']
+Flux_scale = yr2sec * np.array([1.0, -1.0, -1.0])
 Flux_cmap  = 'brewer_BrBG_11'
 
 
@@ -40,7 +51,6 @@ Flux_cmap  = 'brewer_BrBG_11'
 import libs.import_iris #comment out this line if not run on CEH linux box
 import iris
 
-import numpy as np
 from   pylab import sort      
 import matplotlib.pyplot as plt
 
@@ -59,9 +69,9 @@ def load_group(codes, names, dat = None, scale = None, **kw):
     if (dat is None):        
         dat = [load_stash(files, code, name, **kw) for code, name in zip(codes, names)]
         
-       
     for i in range(0, len(dat)):
             if (dat[i].coords()[0].long_name == 'pseudo_level'):
+                print('warning: ' + names[i] + ' has pseudo_levels')
                 dat[i] = dat[i].collapsed('pseudo_level', iris.analysis.MEAN)             
     
     if (scale is not None):
@@ -78,8 +88,7 @@ def load_group(codes, names, dat = None, scale = None, **kw):
 
 
 def plot_cubes(cubes, title, *args):
-    nplots = len(cubes)
-    browser()
+    nplots = len(cubes)    
     plot_cubes_map(cubes, *args)  
     
     plt.subplot(nplots - 1, 2, 4)
@@ -113,6 +122,7 @@ files = sort(listdir_path(data_dir + mod_out))
 
 soil = open_plot_and_return(soil_fignm, soil_title, soil_codes, soil_names,  soil_units, soil_cmap)
 wood = open_plot_and_return(Wood_fignm, Wood_title, Wood_codes, Wood_names,  Wood_units, Wood_cmap)
+wdfl = open_plot_and_return(WdFl_fignm, WdFl_title, WdFl_codes, WdFl_names,  WdFl_units, WdFl_cmap, scale = WdFl_scale)
 
 flux = open_plot_and_return(Flux_fignm, Flux_title, Flux_codes, Flux_names,  Flux_units, Flux_cmap, scale = Flux_scale)
 
@@ -127,7 +137,9 @@ wood = deltaT(wood)
 
 flux.data[0 ] -= flux.data[0 ]
 flux.data[1:] =- flux.data[1:]
+wdfl.data[0 ] -= wdfl.data[0 ]
+wdfl.data[1:] =- wdfl.data[1:]
 
-cmap = ['brewer_RdYlBu_11', 'brewer_PuOr_11', Flux_cmap, 'brewer_RdYlBu_11']
+cmap = ['brewer_RdYlBu_11', 'brewer_PuOr_11', Flux_cmap, Flux_cmap,  'brewer_RdYlBu_11']
 
-open_plot_and_return('overall', 'overall', cmap = cmap, dat = [soil, wood, flux])
+open_plot_and_return('overall', 'overall', cmap = cmap, dat = [soil, wood, wdfl, flux])
