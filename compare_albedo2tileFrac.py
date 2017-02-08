a06e417b3538950f7d3ef38e65fc935fd45a7f99
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-02-08 15:10:43 +0000

    tidying plot: axis titles and labs in right place, title repostion, white space etc

--- compare_albedo2tileFrac.py
+++ compare_albedo2tileFrac.py
@@ -33,17 +33,33 @@ for i in range(0, len(albedos)):
 
     plt.figure(figsize = (15, 15))
     for tile in range(0, tile_f.shape[0]):
-        plt.subplot(5,4,tile)
-        plt.plot(tile_f[tile].data[:], albedo.data[:], "ko")
+        plt.subplot(5,4,tile +1)
+        plt.xlim(0.0, 1.0)
+        plt.ylim(0.0, 1.0)
+        f = plt.plot(tile_f[tile].data[:], albedo.data[:], "ko")
         plt.grid(True)
-        plt.title(tile_n[tile])
+        plt.text(0.5, 0.87, tile_n[tile],
+                 horizontalalignment = 'center', fontsize = 20)
 
-    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.35,
-                        wspace=0.25)
-    git = 'repo: ' + git_info.url + '\n' + 'rev:  ' + git_info.rev
-    plt.gcf().text(.05, .05, git)
+        if (tile / 4 !=  tile / 4.0): plt.tick_params(labelleft   = 'off')
+        if ((tile + 1) / 4 ==  (tile + 1) / 4.0): plt.tick_params(labelright   = 'on')
+
+        if (tile < 4): plt.tick_params(labeltop   = 'on')
+        if (tile < (17 - 4)):  plt.tick_params(labelbottom = 'off')
     
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.07,
+                        wspace=0.09)
+    git = 'repo: ' + git_info.url + '\n' + 'rev:  ' + git_info.rev
+    plt.gcf().text(.5, .05, git)
+
+    
+    plt.gcf().text(.06, .5, albedo_n[i] + ' albedo', rotation = 90,
+                   verticalalignment = 'center', fontsize = 18)
+    
+    plt.gcf().text(.5, .22, 'tile fractional cover',
+                   horizontalalignment = 'center', fontsize = 18)
     fname = 'figs/albedo_' + albedo_n[i] +'.png'
     plt.savefig(fname, bbox_inches = 'tight')
+    
 
 
