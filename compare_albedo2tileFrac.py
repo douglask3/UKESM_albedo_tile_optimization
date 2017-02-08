b03344457a4b24905f4b81ca23eae55350a2bb5d
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-02-08 12:45:01 +0000

    plotting scatter of first level annual average albedo and tile fractions

---
+++ compare_albedo2tileFrac.py
@@ -0,0 +1,46 @@
+import iris
+from libs import git_info
+import numpy as np
+from   os    import listdir, getcwd, mkdir, path, walk
+from   pylab import sort
+from   pdb   import set_trace as browser
+
+import iris.plot as iplt
+import iris.quickplot as qplt
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+data_dir = 'data/'
+
+albedo_file = 'qrclim.land'
+tile_f_file = 'JULES-ES.1p5.vn4.6.S3.dump.19900101.0.n96e_ORCA025.m01s00i216.anc'
+albedo_index = 0
+
+tile_lev = np.array([101  ,102       ,103       ,201  ,202  ,3    ,301  ,302  ,4    ,401  ,402  ,501  ,502  ,6      ,7     ,8          ,9    , 901  ,902   ,903   ,904   ,905   ,906   ,907   ,908   ,909   ,910])
+tile_nme = np.array(['BLD','BLE_Trop','BLE_Temp','NLD','NLE','C3G','C3C','C3P','C4G','C4C','C4P','SHD','SHE','Urban','Lake','Bare Soil','Ice', 'Ice1','Ice2','Ice3','Ice4','Ice5','Ice6','Ice7','Ice8','Ice9','Ice10'])
+
+
+## Load data
+albedo = iris.load(data_dir + albedo_file)[0]
+tile_f = iris.load(data_dir + tile_f_file)[0]
+
+
+albedo = albedo.collapsed('time', iris.analysis.MEAN)
+
+tile_n = tile_f.coord('pseudo_level').points
+tile_n = [tile_nme[tile_lev == i][0] for i in tile_f.coord('pseudo_level').points]
+
+plt.figure(figsize = (15, 15))
+for tile in range(0, tile_f.shape[0]):
+    plt.subplot(5,4,tile)
+    plt.plot(tile_f[tile].data[:], albedo.data[:], "ko")
+    plt.grid(True)
+    plt.title(tile_n[tile])
+
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.45,
+                        wspace=0.35)
+git = 'repo: ' + git_info.url + '\n' + 'rev:  ' + git_info.rev
+plt.gcf().text(.05, .05, git)
+plt.savefig('figs/albedo_test.png', bbox_inches = 'tight')
+
+
--- plot_scaling_file.py
+++ plot_scaling_file.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
 data_dir = 'data/'
-mod_out = 'ag589/'
-stash_code = 'm01s00i289'
 
 albedo_file = 'qrclim.land'
 albedo_index = 0
@@ -20,41 +18,22 @@ albedo_index = 0
 ## Load data
 obs = iris.load(data_dir + albedo_file)[albedo_index]
 
-
-def listdir_path(path):
-    from os import listdir
-    
-    files = listdir(path)
-    files = [path + i for i in files]
-    return files
-
 def define_axes():
     return plt.axes(projection=ccrs.Robinson())
 
-mod_files = sort(listdir_path(data_dir + mod_out))
-stash_contraint = iris.AttributeConstraint(STASH = stash_code)
-mod = iris.load_cube(mod_files, stash_contraint)
-browser()
-## Seasonal tile
-
-## Seaonal grid cell
-mod_season = mod.collapsed('pseudo_level', iris.analysis.MEAN)
 
 ## Annual tile average
-mod_tile = mod.collapsed('time', iris.analysis.MEAN)
+aa = obs.collapsed('time', iris.analysis.MEAN)
 
 ## Annual grid cel avarage
-def plot_aa(dat, plotN):
+def plot_map(dat, plotN):
     plt.subplot(2, 1, plotN, projection=ccrs.Robinson())
     qplt.contourf(dat, 25, cmap = "brewer_Greys_09")
     plt.gca().coastlines()
 
-    
-obs_aa = obs.collapsed('time', iris.analysis.MEAN)
-mod_aa = mod_tile[0,:,:]#.collapsed('pseudo_level', iris.analysis.SUM)
 
-plot_aa(obs_aa, 1)
-plot_aa(mod_aa, 2)
+plot_map(aa, 1)
+## add seasonal plots
 
 plt.show()
 
