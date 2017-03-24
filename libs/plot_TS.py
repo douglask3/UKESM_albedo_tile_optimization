1a8431c5f8b10026bd041d7966289c35a1e21427
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-03-24 16:11:38 +0000

    removinbg first N/2 points for running means as these haven't got the cycle removed

--- libs/plot_TS.py
+++ libs/plot_TS.py
@@ -17,10 +17,10 @@ def grid_area(cube):
 def running_N_mean(l, N):
     sum = 0
     result = list( 0 for x in l)
-
+    
     for i in range( 0, N ):
         sum = sum + l[i]
-        result[i] = sum / (i+1)
+        result[i] = float('nan')#sum / (i+1)
 
     for i in range( N, len(l) ):
         sum = sum - l[i-N] + l[i]
