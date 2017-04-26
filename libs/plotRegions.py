0550fcd3c2784b1efa68b849929afbda0d57bf70
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-04-26 09:35:33 +0100

    adding a 3rd Asia region of just tibetean

--- libs/plotRegions.py
+++ libs/plotRegions.py
@@ -3,11 +3,11 @@ import numpy as np
 from   libs.plot_temporals  import *
 from   libs.ExtractLocation import *
 
-regionNames = ['global', 'NA' , 'NA2', 'Asia', 'Asia2']
-east        = [ None   , 260.0, 275.0,  80.0 ,  80.0  ]
-west        = [ None   , 310.0, 295.0, 105.0 , 105.0  ]
-south       = [ None   ,  50.0,  45.0,  35.0 ,  45.0  ]
-north       = [ None   ,  65.0,  55.0,  50.0 ,  55.0  ]
+regionNames = ['global', 'NA' , 'NA2', 'Asia', 'Asia2', 'Asia3']
+east        = [ None   , 260.0, 275.0,  80.0 ,  80.0  , 80     ]
+west        = [ None   , 310.0, 295.0, 105.0 , 105.0  , 105.0  ]
+south       = [ None   ,  50.0,  45.0,  35.0 ,  45.0  , 35.0   ]
+north       = [ None   ,  65.0,  55.0,  50.0 ,  55.0  , 40.0   ]
 
 def plotRegion(dat, fign, regionName, jobID, levels, units = 'albedo', *args, **kw):
     dat = ExtractLocation(dat, *args, **kw).cubes
@@ -24,5 +24,6 @@ def plotRegion(dat, fign, regionName, jobID, levels, units = 'albedo', *args, **
 def plotAllRegions(dat, nm, *args, **kw):
     for r, e, w, s, n in zip(regionNames, east, west, south, north):
         plotRegion(dat, nm, r, east = e, west = w, south = s, north = n, *args, **kw)
+
 
 
